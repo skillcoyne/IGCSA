99a922a9276150f824d471651daba01f2c6c06bf
Sarah Killcoyne <skillcoyne@gmail.com>
2012-10-19 10:07:59 +0200

    looking at xml files from sky-cgh now

--- ruby/xml-karyotype.rb
+++ ruby/xml-karyotype.rb
@@ -1,11 +1,29 @@
 require 'hpricot'
+require 'yaml'
+require_relative 'lib/sky_karyotype'
 
-xml = File.read('/Users/skillcoyne/Data/sky-cgh/XML/L.Stapleton_1.xml')
+xml_dir = "/Users/skillcoyne/Data/sky-cgh/XML/"
 
-doc = Hpricot.XML(xml)
+Dir.foreach(xml_dir) do |entry|
+  entry = "#{xml_dir}#{entry}"
+  next if File.directory?(entry)
+  puts "Reading #{entry}"
+  xml = File.read(entry)
+  doc = Hpricot.XML(xml)
 
-puts doc.search("//SkyCell/KaryotypeTerms")
+  puts doc.search("//SkyCell/KaryotypeTerms")
 
-karyotype = doc.search("//SkyCellHeader_karyotype").inner_text
+  karyotype = doc.search("//SkyCellHeader_karyotype").inner_text
+  puts karyotype
+  #puts karyotype.split(",")
+end
 
-puts karyotype.split(",")
+#xml = File.read('/Users/skillcoyne/Data/sky-cgh/XML/L.Stapleton_1.xml')
+#
+#doc = Hpricot.XML(xml)
+#
+#puts doc.search("//SkyCell/KaryotypeTerms")
+#
+#karyotype = doc.search("//SkyCellHeader_karyotype").inner_text
+#
+#puts karyotype.split(",")
