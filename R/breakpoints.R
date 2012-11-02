269344962340f94ced52d1ab8c278f4f3680e769
Sarah Killcoyne <skillcoyne@gmail.com>
2012-11-02 16:06:45 +0100

    R script for examining breakpoints from mitelman database.

--- R/breakpoints.R
+++ R/breakpoints.R
@@ -7,14 +7,16 @@ write.table(table(data$Breakpoint), "breakpoint-freq.txt", row.name=FALSE,quote=
 freq = log(table(data$Breakpoint))
 ymax = max(freq)
 
+
+
 # Too many bps, plot by chr - output to PDF at some point
 par(mfrow=c(4,6))
 for (i in c(1:22, c("X", "Y")))
   {
-  print(i)
   nd = subset(data, data$Chr == i)
   freq = table(as.vector(nd$Breakpoint))
-print(freq)
+print(i)
+print(max(freq))
   title = paste("Chr", i, sep=" ")
 #  dev.new()
   plot(log(freq), ylim=c(0, ymax), main=title, xlab="breakpoints", ylab="log(freq)")
