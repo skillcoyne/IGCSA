d8d7feff4504cc7bdd3c9b7ab6801321d42b01f9
Sarah Killcoyne <skillcoyne@gmail.com>
2012-11-05 15:42:38 +0100

    Adding to breakpoint frequency r script.

--- R/breakpoints.R
+++ R/breakpoints.R
@@ -1,11 +1,53 @@
 setwd("C:/Users/LCSB_Student/Data/")
 data = read.table("breakpoints-mm-chr.txt", header=TRUE, sep="\t")
-
 data = data[order(data$Chr),]
-write.table(table(data$Breakpoint), "breakpoint-freq.txt", row.name=FALSE,quote=FALSE, sep="\t")
+allfreq = table(data$Breakpoint)
+ymax = max(log(allfreq))
+allevents = length(allfreq)
 
-freq = log(table(data$Breakpoint))
-ymax = max(freq)
+fm = as.matrix(allfreq)
+
+# simple probability
+fm = cbind(fm, log(fm[,1]), round((fm[,1]/allevents), digits=4))
+colnames(fm) = c('freq', 'log.freq', 'prob')
+fm = as.data.frame(fm)
+write.table(fm, "ind-breakpoint-freq.txt", row.name=TRUE,quote=FALSE, sep="\t")
+
+# p/q 12 appear to be centromeric regions
+# guessing that the regions nearest the centromeres
+# are unstable
+cen = vector(mode="character")
+for (i in c(1:22, c("X", "Y")))
+  {
+  #cen = c(cen, paste(i,"q12", sep=""))
+  #cen = c(cen, paste(i,"p12", sep=""))
+
+  for (b in c(10:13))
+    {
+    cen = c(cen, paste(i, "q", b, sep=""))
+    cen = c(cen, paste(i, "p", b, sep=""))
+    }
+  }
+cent = fm[c(cen),]
+arms = fm[rownames(fm) %in% cen == FALSE,]
+
+a = sum(cent$freq, na.rm=TRUE)
+b = sum(arms$freq, na.rm=TRUE)
+t=a+b
+a/t
+b/t
+
+
+#quarts = fivenum(fm$prob)
+## drop everyting in the bottom quartile as these have a freq below 2
+#fm = fm[fm$prob > quarts[2],]
+
+
+## min/max prob
+# fm[fm$prob == max(fm$prob),]
+# fm[fm$prob == min(fm$prob),]
+
+
 
 
 
@@ -15,10 +57,14 @@ for (i in c(1:22, c("X", "Y")))
   {
   nd = subset(data, data$Chr == i)
   freq = table(as.vector(nd$Breakpoint))
-print(i)
-print(max(freq))
+pn = qnorm(log(freq))
   title = paste("Chr", i, sep=" ")
 #  dev.new()
-  plot(log(freq), ylim=c(0, ymax), main=title, xlab="breakpoints", ylab="log(freq)")
+  plot(pn, ylim=c(0, 1), main=title, xlab="breakpoints", ylab="log(freq)")
+
+#  plot(log(freq), ylim=c(0, ymax), main=title, xlab="breakpoints", ylab="log(freq)")
   }
+
+
+
 
--- ruby/mitelman-bp.r
+++ ruby/mitelman-bp.r
@@ -5,16 +5,25 @@ file = ARGV[0]
 path = File.absolute_path(file).sub!(File.basename(file), "")
 base = File.basename(file, ".txt")
 fout = File.open("#{path}#{base}-chr.txt", 'w')
-fout.write("RefNo\tCaseNo\tBreakpoint\tChr\n")
+fout.write("Breakpoint\tChr\tBand\n")
+skipped = []
 File.open(file, 'r').each_line do |line|
   line.chomp!
-
   cols = line.split("\t")
-  cols[-1] =~ /^([\d|X|Y]{1,2})/
-  fout.write("#{cols.join("\t")}\t#{$1}\n")
 
-
+  # missing the chromosome
+  if (cols[-1].match(/^([\d|X|Y]{1,2})([q|p]\d+)/))
+    #puts line
+    chr = $1; band = $2
+    puts "#{cols[-1]}\t#{chr}\t#{band}\n"
+    fout.write("#{cols[-1]}\t#{chr}\t#{band}\n") #unless cols[-1].match(/or/)
+  else
+    skipped.push(line)
+  end
+  #fout.write("#{cols.join("\t")}\t#{$1}\n")
 end
 
 fout.close
+puts "Skipped: #{skipped.length} that lacked a chromosome, lacked a band or had incorrectly formatted band information."
+
 puts "#{path}#{base}-chr.txt written."
