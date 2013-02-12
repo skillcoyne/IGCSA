864964a5e6055ade01bb227b7b5a61c2bfff2365
Sarah Killcoyne <skillcoyne@gmail.com>
2013-02-12 17:31:59 +0100

    determining variant probabilities

--- R/variation-probabilities.R
+++ R/variation-probabilities.R
@@ -30,28 +30,65 @@ files = list.files()
   print(f)
   
   d = read.table(f, header=T, sep="\t")
+  nrow(d)
+  plot(d$SNP)
   
+  snp_e = unique(d$SNP)
+
+  indel_e = unique(d$INDEL)
+
   snp_f = table(d$SNP)
-  indel_f = table(d$INDEL)
+  #indel_f = table(d$INDEL)
 
-  # doesn't make a difference that I see
-  #counts = vector('numeric', length(snp_f))  
-  #names(counts)=1:length(snp_f)
-  #names(snp_f) = names(counts)
   
-  ks.test(snp_f,pnorm,mean(snp_f),sd(snp_f))
-
-  ks.test(snp_f,ppois,mean(snp_f),sd(snp_f))
+  # poisson does give a good pvalue
+  pt = poisson.test(length(snp_e), mean(snp_e))
   
-  probability_list=pnorm(snp_f,mean(snp_f),sd(snp_f))
-  
-  
-  poisson.test(sum(snp_f), length(snp_f), 0)
-  probability_list=ppois(snp_f,mean(snp_f),sd(snp_f))
-  
+  probability_list=ppois(snp_e,mean(snp_e),sd(snp_e))
   funplot(probability_list)
   
-  write.table(snp_f, row.names=F, quote=F)
+  library(vcd) ## loading vcd package
+  gf <- goodfit(snp_e,type= "poisson",method= "MinChisq")
+  summary(gf)
+  plot(gf,main="Count data vs Poisson distribution")  
+  
+  
+  
+  # Estimate poisson distribution?  
+  #expected=rpois(n=100, lambda=mean(snp_e))
+  pn = 200
+  expected=rpois(n=pn, lambda=pt$estimate)
+  hist(expected, main="Poisson Distribution?")
+  lambda.est = mean(expected) ## estimate of parameter lambda...why???
+  tab.os = table(expected) ## table with empirical frequencies  
+  freq.os<-vector()
+  for(i in 1: length(tab.os)) freq.os[i]<-tab.os[[i]]  ## vector of emprical frequencies
+
+  freq.ex<-(dpois(0:max(expected),lambda=lambda.est)*pn) ## vector of fitted (expected) frequencies
+  acc <- mean(abs(freq.os-trunc(freq.ex))) ## absolute goodness of fit index acc
+  acc/mean(freq.os)*100 ## relative (percent) goodness of fit index
+  
+  h <- hist(expected,breaks=length(tab.os))
+  xhist <- c(min(h$breaks),h$breaks)
+  yhist <- c(0,h$density,0)
+  xfit <- min(expected):max(expected)
+  yfit <- dpois(xfit,lambda=lambda.est)
+  plot(xhist,yhist,type="s",ylim=c(0,max(yhist,yfit)), main="Poison density and histogram")
+  lines(xfit,yfit, col="red")
+  
+  
+  library(vcd) ## loading vcd package
+  gf <- goodfit(expected,type= "poisson",method= "MinChisq")
+  summary(gf)
+  plot(gf,main="Count data vs Poisson distribution")  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   }
--- ruby/read_normal_variations.rb
+++ ruby/read_normal_variations.rb
@@ -10,7 +10,7 @@ end
 config_defaults = YAML.load_file("resources/var.props.example")
 cfg = Utils.check_config(ARGV[0], config_defaults, ['tabix.path', 'chromosome.data'])
 
-Utils.setup_dirs( cfg['output.dir'].split(";"), true)
+Utils.setup_dirs(cfg['output.dir'].split(";"), true)
 output_dir = cfg['output.dir']
 
 chr_info_file = cfg['chromosome.data'] || 'resources/chromosome_gene_info_2012.txt'
@@ -27,10 +27,11 @@ tabix = cfg['tabix'] || 'tabix'
 
 var_dirs = cfg['variation.dir'].split(';')
 var_dirs.each do |dir|
-  warn "#{dir} does not exist" unless File.exists?dir
+  warn "#{dir} does not exist" unless File.exists? dir
   puts "Reading #{dir}"
   Dir.foreach(dir) do |entry|
-    next if entry.start_with?"." or entry.match(/\.tbi$/) # ignore index files which also happen to have 'vcf' in the file name
+    next if File.directory?"#{dir}/#{entry}"
+    next if entry.start_with? "." or entry.match(/\.tbi$/) # ignore index files which also happen to have 'vcf' in the file name
 
     if entry.match(/chr(\d+|X|Y).*\.(vcf)/)
       chr = $1
@@ -38,16 +39,16 @@ var_dirs.each do |dir|
       var_count_file = "#{output_dir}/chr#{chr}-counts.txt"
       vout = File.open(var_count_file, 'w')
       vout.write("# Counts per #{cfg['window']} base pairs\n")
-      vout.write( ['SNP', 'INDEL'].join("\t") + "\n" )
+      vout.write(['SNP', 'INDEL'].join("\t") + "\n")
 
       bins = (chr_info[chr].to_f/cfg['window'].to_f).ceil
       puts "Analyzing chromosome #{chr}, variation window #{cfg['window']}, #{bins} bins..."
 
       (1..bins).each do |win|
-        next unless (win.eql?1 or win%cfg['window']==0)  # this might only work if the window is multiples of 10...
+        next unless (win.eql? 1 or win%cfg['window']==0) # this might only work if the window is multiples of 10...
 
         min = win; max = win+cfg['window']
-        (win.eql?1)? (max -= 1): (min += 1)
+        (win.eql? 1) ? (max -= 1) : (min += 1)
 
         tabix_opt = "#{chr}:#{min}-#{max}"
         vcf_file = "#{output_dir}/chr#{chr}:#{min}-#{max}.vcf"
@@ -55,9 +56,11 @@ var_dirs.each do |dir|
         puts tabix_opt
 
         cmd = "#{tabix} #{dir}/#{entry} #{tabix_opt} > #{vcf_file}"
-        puts cmd
+                                                         #puts cmd
         sys = system("#{cmd}")
-        raise StandardError, "tabix failed to run, please check that it is installed an available in your system path." unless sys
+        unless sys
+          warn "tabix failed to run on #{entry}, please check that it is installed an available in your system path."
+        end
 
         #variants = {}
         snp_count, indel_count = 0, 0
@@ -67,13 +70,14 @@ var_dirs.each do |dir|
           vcf.samples.clear
           #(variants["#{vcf.ref}#{vcf.alt}"] ||= []) << vcf
 
-          snp_count += 1 if vcf.info['VT'].eql?'SNP'
-          indel_count += 1 if vcf.info['VT'].eql?'INDEL'
+          snp_count += 1 if vcf.info['VT'].eql? 'SNP'
+          indel_count += 1 if vcf.info['VT'].eql? 'INDEL'
 
         end
-        vout.write( [snp_count, indel_count].join("\t") + "\n" )
+        #puts [entry, snp_count, indel_count].join("\t")
+        vout.write([snp_count, indel_count].join("\t") + "\n")
 
-        FileUtils.rm_f(vcf_file)  # don't keep the vcf file around
+        FileUtils.rm_f(vcf_file) # don't keep the vcf file around
       end
       vout.close
       puts var_count_file
