62a49f2c5d2654d1848351de1e9adc39615e7353
Sarah Killcoyne <skillcoyne@gmail.com>
2013-03-07 12:45:16 +0100

    more anlysis

--- R/gene-analysis.R
+++ R/gene-analysis.R
@@ -3,26 +3,30 @@ setwd("~/workspace/IGCSA/R")
 source("lib/gc_functions.R")
 source("lib/varplots.R")
 
+
 ## NOTE: Have to redo this with the correct data.  I processed the cpg files badly the first time
 
 dir = "~/Data"
 ens_dir = paste(dir,"/VariationNormal/Frequencies/1000/Ensembl", sep="")
 var_files = list.files(path=ens_dir, pattern="*.txt")
 
-gc_dir = paste(dir, "/VariationNormal/GC/1000", sep="")
-gc_files = list.files(path=gc_dir, pattern="*-gc.txt")
+#gc_dir = paste(dir, "/VariationNormal/GC/1000", sep="")
+#gc_files = list.files(path=gc_dir, pattern="*-gc.txt")
 
 gene_dir = paste(dir, "VariationNormal", sep="/")
-gene_files = list.files(path=gene_dir, pattern="genes-dist2.txt", recursive=T)  
+extremes = "genes-extremes.txt"
+dist2 = "genes-dist2.txt"
 
 gene_perc = vector("numeric", length(var_files))
 gp_names = vector("character", length(var_files))
 
 chr_info = read.table("~/workspace/IGCSA/ruby/resources/chromosome_gene_info_2012.txt", header=T, sep="\t", row.names='Chromosome')
 
-gene_ratio = matrix(ncol=3, nrow=length(var_files))
-colnames(gene_ratio) = c('Total.Fragments', 'Fragment.Genes', 'Known.Genes')
+gr = matrix(ncol=5, nrow=length(var_files))
+colnames(gr) = c('TF.Dist2', 'FG.Dist2', 'TF.Extreme', 'FG.Extreme', 'Known.Genes')
+
 gp_names = vector("character", length(var_files))
+
 #var_files=c("chr3.txt")
 for (i in 1:length(var_files))
   {
@@ -32,32 +36,47 @@ for (i in 1:length(var_files))
   chrnum = sub('chr', "", chr)
   chrdir = paste(dir, "VariationNormal", chr, sep="/")
   
-  # Variation & gc files
-  gc_f = paste(gc_dir, paste(chr, "-gc.txt", sep=""), sep="/")
-  var_f = paste(ens_dir, var_f, sep="/")
-  gene_f = paste(chrdir, "genes-dist2.txt", sep="/")
-  #data = load.data(gc_f, var_f)
-  #var_d = data$vars
-  #gc_d = data$gc
-  gp_names[i] = chr
-  
-  gene_d = read.table(gene_f, header=T, sep="\t")
-  genes = unique(gene_d$Genes) # Since one gene may overlap multiple consecutive regions
+  # gene files
+  df = paste(chrdir, dist2, sep="/")
+  ef = paste(chrdir, extremes, sep="/")
 
-  gene_ratio[i, 'Total.Fragments'] = nrow(gene_d) - nrow(gene_d[gene_d$Position < 10000,])
-  gene_ratio[i, 'Fragment.Genes'] = length(genes)
-  gene_ratio[i, 'Known.Genes'] = chr_info[chrnum, 'Confirmed.proteins']
+  gp_names[i] = chr
+  gr[i, 'Known.Genes'] = chr_info[chrnum, 'Confirmed.proteins']
+  
+  # Dist2
+  gene_d = read.table(df, header=T, sep="\t")
+  genes = unique(gene_d$Genes) # Since one gene may overlap multiple consecutive regions
+  gr[i, 'TF.Dist2'] = nrow(gene_d) - nrow(gene_d[gene_d$Position < 10000,])
+  gr[i, 'FG.Dist2'] = length(genes)
+
+  # Extremes
+  gene_d = read.table(ef, header=T, sep="\t")
+  genes = unique(gene_d$Genes) # Since one gene may overlap multiple consecutive regions
+  gr[i, 'TF.Extreme'] = nrow(gene_d) - nrow(gene_d[gene_d$Position < 10000,])
+  gr[i, 'FG.Extreme'] = length(genes)
   }
-rownames(gene_ratio) = gp_names
+rownames(gr) = gp_names
 
 #gene_perc = sort(gene_perc)
 
 # Proportion of genes in entire chromosome identified within the top of the hill region
-gene_perc = sort( signif(gene_ratio[,2]/gene_ratio[,3], 3)*100 )
+# Number of fragments matters
+cor.test(gr[,2]/gr[,1], 
 
-bp = barplot(gene_perc, names.arg = names(gene_perc), col='blue', ylim=c(0,100))
-#axis(1, at=1:length(gene_perc), labels=names(gene_perc))
-text(bp, gene_perc, labels=paste(gene_perc, '%', sep=""), pos=3 )
-title(main="Genes in dist peak", ylab="Percentage of all genes")
+gr[,4]/gr[,3]
 
 
+sort( signif(gr[,2]/gr[,5], 3)*100 )
+sort( signif(gr[,4]/gr[,5], 3)*100 )
+
+
+
+
+#bp = barplot(gene_perc, names.arg = names(gene_perc), col='blue', ylim=c(0,100))
+
+#text(bp, gene_perc, labels=paste(gene_perc, '%', sep=""), pos=3 )
+#title(main="Genes in dist peak", ylab="Percentage of all genes")
+
+#plot(gr[,2], type='h', col='red', xaxt='n')
+#lines(gr[,4], type='h', col='blue')
+#axis(1, at=1:nrow(gr), labels=rownames(gr))
--- R/snv-prob.R
+++ R/snv-prob.R
@@ -1,12 +1,10 @@
 
-source("lib/gc_functions.R")
 
 prob.snv<-function(df)
-  { # this isn't right come back to it
+  { 
   df$prob =  df[,2]/sum(df[,2] ) 
   return(df)
   }
-
 
 dir = "~/Data/VariationNormal/Frequencies"
 
@@ -28,37 +26,6 @@ newd = newd[,-2]
 #write.table(newd, quote=F, sep="\t", col.names=F, row.names=F)
 write.table(newd, quote=F, sep="\t", col.names=F, row.names=F, file=paste(prob_dir, "snp-prob.txt", sep="/"))
 rm(a,c,g,t,d,newd)
-
-
-## These probabilities are in addition to the probability of each variation type, determined per chromosome
-sizes = c(1,10,100,1000)
-files = list.files(path=dir, pattern="*length-count.txt")
-for (file in files)
-  {
-  filename = sub("^.*\\/", "", file)
-  variation = sub("-length-count.txt", "", filename)
-
-  d = read.table( paste(dir, file, sep="/"), header=T, sep="\t")
-  
-  probs = matrix(ncol=2, nrow=length(sizes))
-  probs[,1] = sizes
-  
-  for( i in 1:length(sizes) )
-    {
-    if (i == 1) chunk = d[d[,1] == sizes[i],]
-    if (i > 1 ) chunk = d[d[,1] <= sizes[i] & d[,1] > sizes[i-1],]
-    
-    probs[i,2] = sum(chunk[,2])/sum(d[,2])
-    }
-  filename = paste(variation, "-prob.txt", sep="")
-  write.table(probs, quote=F, col.names=F, row.names=F, sep="\t", file=paste(prob_dir, filename, sep="/"))
-  #print(variation)
-  #write.table(probs, quote=F, col.names=F, row.names=F, sep="\t")
-  rm(probs, d, chunk)
-  }
-
-
-
 
 
 
