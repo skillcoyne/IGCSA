a2dd34f8542418e15f2a7a04623899329b708b7c
Sarah Killcoyne <skillcoyne@gmail.com>
2014-09-28 20:27:54 +0200

    updated plots

--- R/alignment/evaluation.R
+++ R/alignment/evaluation.R
@@ -1,17 +1,24 @@
 library('mclust')
 
 row.gen<-function(df)
-{
+  {
 	row = cbind(nrow(df), 
-	mean(log(df$len)), 
-	sd(log(df$len)),
-	mean(df$phred),
-	sd(df$phred),
-	mean(df$mapq),
-	sd(df$mapq),
-	nrow( df[df$ppair == TRUE,] )/nrow(df))
+	  mean(log(df$len)), 
+	  sd(log(df$len)),
+	  mean(df$phred),
+	  sd(df$phred),
+	  mean(df$mapq),
+	  sd(df$mapq),
+	  nrow( df[df$ppair == TRUE,] )/nrow(df),
+	  length(which(df$orientation == 'F:F')),
+	  length(which(df$orientation == 'F:R')),
+	  length(which(df$orientation == 'R:R')),
+	  length(which(df$orientation == 'R:F'))
+    )
+  
+  
 	return(row)
-}
+  }
 
 sort.by<-function(df, col)
 	{
@@ -52,7 +59,7 @@ setwd(paste(getwd(), "alignments/HCC1954", sep="/"))
 
 read_files = list.files(pattern="*paired_reads.txt", recursive=T)
 
-cols = c('nreads', 'mean.dist', 'sd.dist', 'mean.phred', 'sd.phred', 'mean.mapq','sd.mapq', 'pp.ratio')
+cols = c('nreads', 'mean.dist', 'sd.dist', 'mean.phred', 'sd.phred', 'mean.mapq','sd.mapq', 'pp.ratio', 'FF','FR','RR','RF')
 rAll = as.data.frame(matrix(ncol=length(cols),nrow=length(read_files),data=0,
                             dimnames=list(dirname(read_files), cols)))
 rLeft = rAll
@@ -67,71 +74,80 @@ emmodel = list()
 if (savePlot) pdf(file="Rplots.pdf", onefile=T)
 for (file in read_files)
 	{
-print(file)
-name = dirname(file)
-reads = read.table(file, header=T, sep="\t")
-reads$cigar = as.character(reads$cigar)
-reads$cigar.total = cigar.len(reads$cigar)
-reads$orientation = as.character(reads$orientation)
+  print(file)
+  name = dirname(file)
+  result = tryCatch({
+    reads = read.table(file, header=T, sep="\t")
+  
+    reads$cigar = as.character(reads$cigar)
+    reads$cigar.total = cigar.len(reads$cigar)
+    reads$orientation = as.character(reads$orientation)
 
-#plot(sort(reads$cigar.total), type='h')
-length(which(reads$cigar.total < 0))
-nrow(reads)
-reads = reads[reads$cigar.total > 0,]
+    length(which(reads$cigar.total < 0))
+    nrow(reads)
+    reads = reads[reads$cigar.total > 0,]
 
-# percentage of paired reads with > 0 read distance that are 'proper pairs'
-print(nrow( reads[reads$ppair == TRUE,] )/nrow(reads))
-summary(reads$len)
+    # percentage of paired reads with > 0 read distance that are 'proper pairs'
+    print(nrow( reads[reads$ppair == TRUE,] )/nrow(reads))
+    summary(reads$len)
 
-model = getMixtures(log(reads$len), "V")
-emmodel[[name]] = model
+    model = getMixtures(log(reads$len), "V")
+    emmodel[[name]] = model
 
-counts = log(reads$len)
-hist(counts, breaks=100, col="lightblue", border=F, prob=T, xlim=c(3,18),
-     main=name, sub=paste("Ratio of pp:all paired=", round(nrow( reads[reads$ppair == TRUE,] )/nrow(reads),2)))
-lines(density(counts, kernel="gaussian"), col="blue")
+    png_file=paste(getwd(), name, "log-counts.png", sep="/")
+    png(filename=png_file, width=800, height=600)
+    counts = log(reads$len)
+    hist(counts, breaks=100, col="lightblue", border=F, prob=T, xlim=c(3,18), xlab="log(read-pair distance)", main=name)
+    lines(density(counts, kernel="gaussian"), col="blue")
 
-for (i in 1:ncol(model$z))
-  { 
-  m = model$parameters$mean[i]
-  v = model$parameters$variance$sigmasq[i] 
-  abline(0,0,v=m); 
-  text(m, 0.1, labels=paste("em:",round(m,2)), pos=2)
-  text(m, 0.05, labels=round( mean(model$z[,i]),3 ), pos=2)
+    for (i in 1:ncol(model$z))
+      { 
+      m = model$parameters$mean[i]
+      v = model$parameters$variance$sigmasq[i] 
+      abline(0,0,v=m)
+      text(m, 0.1, labels=paste("mean:",round(m,2)), pos=2)
+      text(m, 0.05, labels=paste("score:", round( mean(model$z[,i]),3 )), pos=2)
+      }
+  dev.off()
+
+  rt = as.integer(which(model$parameters$mean == max(model$parameters$mean)))
+  lt =  as.integer(which(model$parameters$mean != max(model$parameters$mean)))
+
+  m = model$parameters$mean[lt]
+  v = model$parameters$variance$sigmasq[lt] 
+  leftD = reads[ counts >= (m-v) & counts <= (m+v) ,]
+  #hist(leftD$cigar.total)
+
+  m = model$parameters$mean[rt]
+  v = model$parameters$variance$sigmasq[rt] 
+  rightD = reads[ counts >= (m-v) & counts <= (m+v) ,]
+  #hist(rightD$cigar.total)
+
+  #plot(sort(rightD$cigar.total), col='red', type='l', ylab='cigar total')
+  #lines(sort(leftD$cigar.total), col='blue', type='l')
+
+  breakpoint = bands[ which(paste(bands$chr,bands$band,sep="") %in% unlist(strsplit(basename(name), "-"))), 'length'][1]
+
+  left = reads[reads$pos <= breakpoint & reads$mate.pos <= breakpoint,]
+  nrow(left)
+  right = reads[reads$pos >= breakpoint & reads$mate.pos >= breakpoint,]
+  nrow(right)
+
+  crossing = reads[reads$pos <= breakpoint & reads$mate.pos >= breakpoint,]
+  nrow(crossing)
+
+  rAll[name,] = row.gen(reads)
+  rLeft[name,] = row.gen(left)
+  rRight[name,] = row.gen(right)
+  rSpan[name,] = row.gen(crossing)
+  
+  }, error = function(err) {
+    print(paste("Failed to read file", file))
+    warning(err)
+    cat(paste("Failed to read file", file), file="errors.txt", sep="\n", append=T)
+    cat(paste(err, collapse="\n"), file="errors.txt", append=T)
+  })
   }
-
-rt = as.integer(which(model$parameters$mean == max(model$parameters$mean)))
-lt =  as.integer(which(model$parameters$mean != max(model$parameters$mean)))
-
-m = model$parameters$mean[lt]
-v = model$parameters$variance$sigmasq[lt] 
-leftD = reads[ counts >= (m-v) & counts <= (m+v) ,]
-#hist(leftD$cigar.total)
-
-m = model$parameters$mean[rt]
-v = model$parameters$variance$sigmasq[rt] 
-rightD = reads[ counts >= (m-v) & counts <= (m+v) ,]
-#hist(rightD$cigar.total)
-
-#plot(sort(rightD$cigar.total), col='red', type='l', ylab='cigar total')
-#lines(sort(leftD$cigar.total), col='blue', type='l')
-
-breakpoint = bands[ which(paste(bands$chr,bands$band,sep="") %in% unlist(strsplit(basename(name), "-"))), 'length'][1]
-
-left = reads[reads$pos <= breakpoint & reads$mate.pos <= breakpoint,]
-nrow(left)
-right = reads[reads$pos >= breakpoint & reads$mate.pos >= breakpoint,]
-nrow(right)
-
-crossing = reads[reads$pos <= breakpoint & reads$mate.pos >= breakpoint,]
-nrow(crossing)
-
-rAll[name,] = row.gen(reads)
-rLeft[name,] = row.gen(left)
-rRight[name,] = row.gen(right)
-rSpan[name,] = row.gen(crossing)
-
-}
 dev.off()
 
 
@@ -161,12 +177,22 @@ right = unlist(lapply(emmodel, right.dist))
 #summary(right[right < 0.3])
 #rSpan[names(right[right >= 0.5]),]
 
-top_pairs = right[right >= 0.5]
-sort(top_pairs, decreasing=T)
-#sort(top_pairs[grep('^.*/(8.*|5.*)', names(top_pairs), value=T)])
+km = kmeans(right, 4)
 
-#hist(right)
-#kmeans(right, 4)
-write.table(sort(right, decreasing=), quote=F, sep="\t", col.name=F, file="scores.txt")
+png(filename="scores.png", width=1200, height=800)
+par(mfrow=c(2,1))
+hist(right, breaks=30,
+     main="Scores for 'right' distribution", xlab="EM scores per pair", col='blue', border='grey')
 
+right=sort(right)
+plot(right, col=km$cluster[names(right)], pch=19, ylab='scores',xlab='reference pairs')
+points(km$centers, col = 1:4, pch=8)
+text(km$center, labels=round(km$centers, 3), pos=4)
+dev.off()
 
+# top cluster
+top_pairs = sort(right[ right >= max(km$centers) ], decreasing=T)
+
+write.table(top_pairs, quote=F, sep="\t", col.name=F, file="top_pair_scores.txt")
+top = rSpan[names(top_pairs),]
+print(top_pairs)
