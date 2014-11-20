3798d3bd6de506488a82db8e97fcaf279f184e05
Sarah Killcoyne <skillcoyne@gmail.com>
2014-11-20 11:00:57 +0100

    adding more info to the summary

--- R/alignment/read_distance_analysis.R
+++ R/alignment/read_distance_analysis.R
@@ -2,9 +2,8 @@ source("~/workspace/IGCSA/R/alignment/lib/read_eval.R")
 
 args <- commandArgs(trailingOnly = TRUE)
 
-#args[1] = "/Volumes/exHD-Killcoyne/Insilico/runs/alignments/Random/HCC1954.G31860/12q13-5q15"
-
-#args[2] = "/Volumes/exHD-Killcoyne/TCGA/sequence/cell_lines/HCC1954.G31860/G31860.HCC1954.6.bam"
+args[1] = "/Volumes/exHD-Killcoyne/Insilico/runs/alignments/Random/HCC1954.G31860/15q15-4q13"
+args[2] = "/Volumes/exHD-Killcoyne/TCGA/sequence/cell_lines/HCC1954.G31860/G31860.HCC1954.6.bam"
 
 if (length(args) < 2)
   stop("Missing required arguments: <directory to read in> <original aligned bam: OPTIONAL>")
