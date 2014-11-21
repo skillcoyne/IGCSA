79dc9335c8b1ca321ac8945be899d341e2029d13
Sarah Killcoyne <skillcoyne@gmail.com>
2014-11-21 12:05:27 +0100

    bug in calculation of normal distance

--- R/alignment/read_distance_analysis.R
+++ R/alignment/read_distance_analysis.R
@@ -2,8 +2,8 @@ source("~/workspace/IGCSA/R/alignment/lib/read_eval.R")
 
 args <- commandArgs(trailingOnly = TRUE)
 
-args[1] = "/Volumes/exHD-Killcoyne/Insilico/runs/alignments/Random/HCC1954.G31860/15q15-4q13"
-args[2] = "/Volumes/exHD-Killcoyne/TCGA/sequence/cell_lines/HCC1954.G31860/G31860.HCC1954.6.bam"
+#args[1] = "/Volumes/exHD-Killcoyne/Insilico/runs/alignments/Random/HCC1954.G31860/15q15-4q13"
+#args[2] = "/Volumes/exHD-Killcoyne/TCGA/sequence/cell_lines/HCC1954.G31860/G31860.HCC1954.6.bam"
 
 if (length(args) < 2)
   stop("Missing required arguments: <directory to read in> <original aligned bam: OPTIONAL>")
@@ -18,9 +18,8 @@ if (!is.null(args[2]))
   cigar = orig$cigar
   
   print(summary(distances))
-  print(summary(phred))
   }
-
+distances = distances[-which(distances > median(distances)*2)]
 
 read_file = list.files(path=args[1], pattern="*paired_reads.txt", recursive=T)
 summary = analyze.reads(paste(args[1],read_file,sep="/") , mean(distances), sd(distances), mean(phred) )
